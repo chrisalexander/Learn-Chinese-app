0617725f2723556d3e79c62ccf9aec5cef215f52
Chris Alexander <chris@chris-alexander.co.uk>
2015-01-16 21:45:58 +0000

    Prism and MEF composition for views / view models

---
+++ DBManager/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Composition;
+
+namespace DBManager.ViewModels
+{
+    /// <summary>
+    /// View model for the main page.
+    /// </summary>
+    [Export(typeof(MainPageViewModel))]
+    public class MainPageViewModel
+    {
+        /// <summary>
+        /// MEF importing constructor.
+        /// </summary>
+        [ImportingConstructor]
+        public MainPageViewModel()
+        {
+            
+        }
+    }
+}
---
+++ DBManager/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+using Windows.UI.Xaml.Controls;
+
+namespace DBManager.Views
+{
+    /// <summary>
+    /// The view code for the main page.
+    /// </summary>
+    public sealed partial class MainPage : Page, IView
+    {
+        /// <summary>
+        /// Constructor for the main page.
+        /// </summary>
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
