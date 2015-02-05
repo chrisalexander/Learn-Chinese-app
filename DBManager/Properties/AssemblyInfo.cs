ea5d127312f9b6c1faf05eac27e9aed2c3562be0
Chris Alexander <chris@chris-alexander.co.uk>
2015-02-05 18:18:40 +0000

    Icons and colours

--- DBManager/Properties/AssemblyInfo.cs
+++ DBManager/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DBManager")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Database manager for language files")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DBManager")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Chris Alexander 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
