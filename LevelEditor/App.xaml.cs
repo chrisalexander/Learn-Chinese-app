478b4f0cbd37e82a5174af5e7b940a9c75007c0c
Chris Alexander <chris@chris-alexander.co.uk>
2015-02-14 11:45:51 +0000

    Initial level editor template

---
+++ LevelEditor/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using LangDB;
+using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Composition.Hosting;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+
+namespace LevelEditor
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : MvvmAppBase
+    {
+        /// <summary>
+        /// The container for MEF.
+        /// </summar>y
+        private CompositionHost container;
+
+        /// <summary>
+        /// List of the assembly modules to include in the container.
+        /// </summary>
+        private IEnumerable<Type> assemblyModules = new[] { typeof(App), typeof(LangDBModule) };
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Application launch callback.
+        /// </summary>
+        /// <param name="args">Launch args.</param>
+        /// <returns>When complete.</returns>
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            var configuration = new ContainerConfiguration().WithAssemblies(this.assemblyModules.Select(t => t.GetTypeInfo().Assembly));
+
+            this.container = configuration.CreateContainer();
+            this.container.SatisfyImports(this);
+
+            ViewModelLocationProvider.SetDefaultViewModelFactory(t => this.container.GetExport(t));
+
+            NavigationService.Navigate("Main", null);
+            return Task.FromResult<object>(null);
+        }
+    }
+}
---
+++ LevelEditor/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+using System.Composition;
+
+namespace LevelEditor.ViewModels
+{
+    /// <summary>
+    /// View model for the main page.
+    /// </summary>
+    [Export(typeof(MainPageViewModel))]
+    public class MainPageViewModel : ViewModel
+    {
+        /// <summary>
+        /// MEF importing constructor.
+        /// </summary>
+        [ImportingConstructor]
+        public MainPageViewModel()
+        {
+        }
+    }
+}
