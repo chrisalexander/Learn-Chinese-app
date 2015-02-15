8961b9b828fa78277a61ea87e6db9bfae7963c1c
Chris Alexander <chris@chris-alexander.co.uk>
2015-02-15 18:21:08 +0000

    Course database service

---
+++ CourseDB/Services/CourseDatabaseService.cs
@@ -0,0 +1,72 @@
+﻿using CourseDB.Model;
+using DBUtils.Services;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Pickers;
+
+namespace CourseDB.Services
+{
+    /// <summary>
+    /// Service for manipulating course databases.
+    /// </summary>
+    [Export(typeof(ICourseDatabaseService))]
+    public class CourseDatabaseService : AbstractFileServiceWrapper<CourseDatabase, ICourseDatabase>, ICourseDatabaseService
+    {
+        /// <summary>
+        /// The database file service.
+        /// </summary>
+        private readonly IDatabaseFileService<ICourseDatabase> fileService;
+
+        /// <summary>
+        /// The supported file extensions of course databases.
+        /// </summary>
+        private readonly IEnumerable<string> extensions = new[] { ".coursedb" };
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="fileService">The file service.</param>
+        [ImportingConstructor]
+        public CourseDatabaseService(IDatabaseFileService<ICourseDatabase> fileService)
+        {
+            this.fileService = fileService;
+        }
+
+        /// <summary>
+        /// Provide the file service to the abstract wrapper.
+        /// </summary>
+        protected override IDatabaseFileService<ICourseDatabase> FileService
+        {
+            get
+            {
+                return this.fileService;
+            }
+        }
+
+        /// <summary>
+        /// Provide the default picker location to the abstract wrapper.
+        /// </summary>
+        protected override PickerLocationId DefaultPickerLocation
+        {
+            get
+            {
+                return PickerLocationId.DocumentsLibrary;
+            }
+        }
+
+        /// <summary>
+        /// Provide the supported extensions to the abstract wrapper.
+        /// </summary>
+        protected override IEnumerable<string> Extensions
+        {
+            get
+            {
+                return this.extensions;
+            }
+        }
+    }
+}
---
+++ CourseDB/Services/ICourseDatabaseService.cs
@@ -0,0 +1,12 @@
+﻿using CourseDB.Model;
+using DBUtils.Services;
+
+namespace CourseDB.Services
+{
+    /// <summary>
+    /// Inteface for course database manipulation services.
+    /// </summary>
+    public interface ICourseDatabaseService : IFileServiceWrapper<CourseDatabase, ICourseDatabase>
+    {
+    }
+}
