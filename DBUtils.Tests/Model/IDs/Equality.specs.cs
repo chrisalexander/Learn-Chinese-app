7ac53c19dea75cba75e1088c8059c8abeac571c1
Chris Alexander <chris@chris-alexander.co.uk>
2015-03-15 11:01:14 +0000

    First pass at abstract ID and test

---
+++ DBUtils.Tests/Model/IDs/Equality.specs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using DBUtils.Model;
+using Machine.Specifications;
+
+namespace DBUtils.Tests.Model.IDs
+{
+    [Subject(typeof(AbstractId))]
+    public class When_comparing_root_and_descendant
+    {
+        Establish Context = () =>
+        {
+            var guid = Guid.NewGuid();
+            Descendant = new TestDescendantAbstractId(null, guid);
+            Id = new TestAbstractId(guid);
+        };
+
+        // ReSharper disable once SuspiciousTypeConversion.Global
+        It Should_be_equal = () => Descendant.Equals(Id).ShouldBeTrue();
+
+        // ReSharper disable once SuspiciousTypeConversion.Global
+        It Should_be_equal_the_other_way = () => Id.Equals(Descendant).ShouldBeTrue();
+
+        private static TestDescendantAbstractId Descendant;
+
+        private static TestAbstractId Id;
+    }
+
+
+    public class TestAbstractId : AbstractId
+    {
+        public TestAbstractId(Guid root)
+        {
+            this.RootId = root;
+        }
+    }
+
+    public class TestDescendantAbstractId : DescendantAbstractId<TestAbstractId>
+    {
+        public TestDescendantAbstractId(TestAbstractId parent, Guid root)
+        {
+            this.ParentId = parent;
+            this.RootId = root;
+        }
+    }
+}
---
+++ DBUtils/Model/AbstractId.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DBUtils.Model
+{
+    /// <summary>
+    /// Base class for all IDs.
+    /// </summary>
+    public abstract class AbstractId
+    {
+        /// <summary>
+        /// Allow implementers read access to the root GUID.
+        /// </summary>
+        public Guid RootId { get; protected set; }
+
+        /// <summary>
+        /// Equals comparison.
+        /// </summary>
+        /// <param name="obj">The object to compare to.</param>
+        /// <returns>Whether the objects are the equal.</returns>
+        public override bool Equals(object obj)
+        {
+            var targetId = obj as AbstractId;
+
+            return targetId != null && this.GetHashCode().Equals(targetId.GetHashCode());
+        }
+
+        /// <summary>
+        /// Gets the hash code.
+        /// </summary>
+        /// <returns>The hash code.</returns>
+        public override int GetHashCode()
+        {
+            return this.RootId.GetHashCode();
+        }
+
+        /// <summary>
+        /// Provides a string format of the ID.
+        /// </summary>
+        /// <returns>The ID as a string.</returns>
+        public override string ToString()
+        {
+            return this.RootId.ToString();
+        } 
+    }
+}
